bde2c445283579f0960a5225ae7f600b27b33c84
nipunperera01 <136310372+nipunperera01@users.noreply.github.com>
2023-07-17 12:54:52 +0530

    Add files via upload

---
+++ LabC2L_205_2023.7.17/Q10/src/main/java/com/mycompany/q10/Q10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q10;
+
+/**
+ *
+ * @author USER
+ */
+public class Q10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ LabC2L_205_2023.7.17/Q9/src/main/java/com/mycompany/lab9/Lab9.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab9;
+
+/**
+ *
+ * @author USER
+ */
+public class Lab9 {
+
+    public static void main(String[] args) {
+        gui2 n=new gui2();
+        n.show();
+    }
+}
